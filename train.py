20610c710b622ad851c3dda79f1e9e49866f1160
Yohaï-Eliel BERREBY <me@yberreby.com>
2025-01-24 14:16:12 -0500

    add train notebook scaffolding

---
+++ train.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.6
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+# %load_ext autoreload
+# %autoreload 2
+
+# %%
+import src
+
+# %%
