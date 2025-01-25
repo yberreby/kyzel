eff345f9b32a1aa811a1aff58b6ae65cced6252b
Yohaï-Eliel BERREBY <me@yberreby.com>
2025-01-25 18:14:38 -0500

    train nb: add LoRA unsloth patching

--- nb/train.py
+++ nb/train.py
@@ -30,13 +30,30 @@ import src
 from unsloth import FastLanguageModel
 import torch
 
-max_seq_length = 2048
+# phi4 baseline -> 10.5 GB
+max_seq_length = 4000
 model_name = "unsloth/Phi-4"
 
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name=model_name,
     max_seq_length=max_seq_length,
     load_in_4bit=True,
+)
+
+# %%
+# Do model patching and add fast LoRA weights
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 1,
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 64,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    max_seq_length = max_seq_length,
 )
 
 # %%
